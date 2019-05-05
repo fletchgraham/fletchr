cd0df1541027a2046b990389d385bc16a75db6df
fletchgraham <sfletchgraham@gmail.com>
2019-05-05 19:55:53 -0400

    tried to add delete functionality but it isn't workin.

--- fletchr/blog.py
+++ fletchr/blog.py
@@ -43,3 +43,60 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is requred'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title=?, body=? WHERE id=?', (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post = post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    print('id is : ' + str(id))
+    db = get_db()
+    print('connected to db')
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    print('deleted ' + str(id))
+    return redirect(url_for('blog.index'))
+
+# Helpers
+
+def get_post(id, check_author=True):
+    # check author is in case we ever want to get a post without checking the author.
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+    print('got ' + str(post['id']))
+
+    if post is None:
+        abort(404, "Post id {0} doesn't exist".format(id))
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
